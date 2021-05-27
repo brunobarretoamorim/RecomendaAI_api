103bd4f37bedb8bc55b6ea3474cbff4fa03c553c
brunobarretoamorim <33237659+brunobarretoamorim@users.noreply.github.com>
2021-05-26 22:50:03 -0300

    Tratamento de LC e seleção de K = 7 no Knn

--- carregar_modelo.py
+++ carregar_modelo.py
@@ -1,3 +1,4 @@
+from os import error
 import pandas as pd
 import pickle
 import random
@@ -7,13 +8,13 @@ from werkzeug.exceptions import HTTPException, BadRequest
 class NumeroRespostaIncorreta(HTTPException):
     code = 506
     name = "Numero de respostas incorretas"
-    description = 'O valor de competencias informado está incorreto, lembre que são necessários 30 competencias para análise'
+    description = 'O valor de respostas informado está incorreto, lembre que são necessários 40 respostas para cada prova'
 
 
 class MateriaInvalida(HTTPException):
     code = 507
     name = "Materia invalida"
-    description = 'As materias validas são: [CH, CN, LC, MT]'
+    description = 'As materias validas são: [ch, cn, lc, mt]'
 
 class ErroCarregarModelo(HTTPException):
     code = 508
@@ -22,11 +23,11 @@ class ErroCarregarModelo(HTTPException):
 
 
 def carregaBase(materia, respostas = [], *args):
-
-    if materia == ('MT'):
+    
+    if materia == 'MT':
         try:
-            modelo = open("modelos/modelo_recsys_mt","rb")
-            df = pd.read_parquet("dataset/erroshabilidadesmt_grp.parquet")
+            modelo = open('modelos/modelo_recsys_mt','rb')
+            df = pd.read_parquet('dataset/erroshabilidadesmt_grp.parquet')
         except:
             raise ErroCarregarModelo()
     elif materia == 'CN':
@@ -43,11 +44,11 @@ def carregaBase(materia, respostas = [], *args):
         except:
             raise ErroCarregarModelo()
     elif materia == 'LC':
-            try:
-                modelo = open('modelos/modelo_recsys_lc','rb')
-                df = pd.read_parquet('dataset/erroshabilidadeslc_grp.parquet')
-            except:
-                raise ErroCarregarModelo()
+        try:
+            modelo = open('modelos/modelo_recsys_lc','rb')
+            df = pd.read_parquet('dataset/erroshabilidadeslc_grp.parquet')
+        except error as e:
+            print(e)
     else:
         raise MateriaInvalida()
 
@@ -55,34 +56,32 @@ def carregaBase(materia, respostas = [], *args):
         lm_new = pickle.load(modelo)
         modelo.close()
         a = pd.DataFrame(respostas).T
+        a.rename(index = {0:'Aluno_Bot'},inplace=True)
         _,index = lm_new.kneighbors(a)
         df_filtrado = df.iloc[index[0]]
+        a.columns = df_filtrado.columns
+        df_filtrado = pd.concat([df_filtrado,a])
         return df_filtrado
     except:
-        return "erro"
-        #raise Exception()
-        #raise NumeroRespostaIncorreta()
+        raise NumeroRespostaIncorreta()
         
 
 def retornaHabilidades(x):
     try:
+        inscricao = 'Aluno_Bot'
         habilidades_erros = {}
-        valores = []
-        habilidades = []
         for coluna in x.columns:
-            moda = int(x[coluna].mode())
-            if moda >= 1:
-                habilidades_erros[coluna] = moda
-                #valores.append(moda)
-                #habilidades.append(coluna)
-        return habilidades_erros#(valores,habilidades)
+            media = int(x[coluna].mean())
+            if media >= 1:
+                habilidades_erros[coluna] = (x.loc[inscricao][coluna],media)
+
+        return habilidades_erros
     except:
-        return print("Máteria invalida")
+        return print('Matéria Inválida')
 
 
 def main(materia, respostas):
     df_filtrado = carregaBase(materia, respostas)
     habilidades_dc = retornaHabilidades(df_filtrado)
-
+    print('rodou carregar modelo')
     return habilidades_dc
-
--- criacao_modelo.py
+++ criacao_modelo.py
@@ -1,9 +1,9 @@
 import pickle
 import pandas as pd
-df = pd.read_parquet('dataset/erroshabilidadeslc_grp.parquet')
+df = pd.read_parquet('erroshabilidadeslc_grp.parquet')
 
 from sklearn.neighbors import NearestNeighbors
-knn = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=40, n_jobs=-1)
+knn = NearestNeighbors(metric='cosine', algorithm='auto', n_neighbors=7, n_jobs=-1)
 knn.fit(df)
 
 arquivo = open('modelos/modelo_recsys_lc','wb')
